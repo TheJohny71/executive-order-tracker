8f8a75949c46ac128688728c01f87edd60c6b7b6
TheJohny71 <jduval71@me.com>
2025-02-08 23:21:07 +0000

    update

---
+++ src/components/loading/LoadingFallback.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const LoadingFallback = () => (
+  <div className="min-h-screen bg-gray-50 p-4">
+    <div className="max-w-7xl mx-auto">
+      <Card className="mb-4">
+        <CardHeader>
+          <Skeleton className="h-8 w-64" />
+          <Skeleton className="h-4 w-96 mt-2" />
+        </CardHeader>
+      </Card>
+      
+      {[1, 2, 3].map((i) => (
+        <Card key={i} className="mb-4">
+          <CardHeader>
+            <Skeleton className="h-6 w-full max-w-md" />
+          </CardHeader>
+          <CardContent>
+            <Skeleton className="h-24 w-full" />
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  </div>
+);

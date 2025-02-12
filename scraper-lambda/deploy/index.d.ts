216b7cf7c21231670dd66b57e6220d67a5f1fbb4
TheJohny71 <jduval71@me.com>
2025-02-12 16:23:57 +0000

    updates

---
+++ scraper-lambda/deploy/index.d.ts
@@ -0,0 +1,6 @@
+import { LambdaEvent } from './types.js';
+export declare const handler: (_event: LambdaEvent) => Promise<{
+    statusCode: number;
+    headers: Record<string, string>;
+    body: string;
+}>;
